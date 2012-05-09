46cde4da31748ee18a61fb8e5a5ac54aecf2164b
Rehno Lindeque <errantkid@gmail.com>
2012-05-08 23:47:43 -0700

    Created a project for generic adt utilities

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# adt-util.js
+
+Practical, generic utilities for working with [adt.js](https://github.com/rehno-lindeque/adt.js).
---
+++ src/header.js
@@ -0,0 +1,9 @@
+/*
+ * adt-util.js - Algebraic Data Type utilities for JavaScript
+ * adt-util.js is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
+ * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
+ * Use it in combination with https://github.com/rehno-lindeque/adt.js
+ */
+var adt = adt || {};
+(function() {
+"use strict";
